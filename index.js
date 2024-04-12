f33cfa5d598a293aec90d9e8dded1003457b06ee
Adam Cowley <adam@adamcowley.co.uk>
2024-04-12 13:15:22 +0100

    init

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { RunnableSequence } from "@langchain/core/runnables"
+import { PromptTemplate } from "@langchain/core/prompts"
+import { ChatOpenAI } from "@langchain/openai"
+import { StringOutputParser } from "@langchain/core/output_parsers"
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { Neo4jGraphQL } from "@neo4j/graphql";
@@ -6,20 +10,6 @@ import { config } from "dotenv"
 
 // Load .env file
 config()
-
-// Define type definitions
-const typeDefs = `#graphql
-  type Movie {
-    title: String
-    plot: String
-    actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
-  }
-
-  type Actor {
-    name: String
-    movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
-  }
-`;
 
 // Create driver instance
 const driver = neo4j.driver(
@@ -30,12 +20,94 @@ const driver = neo4j.driver(
   )
 );
 
-// Create schema
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+// Type Defs
+const typeDefs = `
+  interface CanGenerate {
+    generate(prompt: String!): GeneratedResponse!
+  }
+
+  type GeneratedResponse {
+    text: String!
+  }
+
+  type Movie implements CanGenerate {
+    title: String!
+    plot: String!
+    generate(prompt: String!): GeneratedResponse! @customResolver
+  }
+
+
+  type ActedIn @relationshipProperties {
+    role: String
+}
+
+  type Actor implements CanGenerate {
+    name: String!
+    born: Date
+    actedInMovies: [Movie!]! @relationship(type: "ACTED_IN", properties: "ActedIn", direction: OUT)
+
+
+    generate(prompt: String!): GeneratedResponse! @customResolver
+  }
+`;
+
+// Generate function
+const generate = async (source, args) => {
+  const prompt = PromptTemplate.fromTemplate(args.prompt)
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    model: args.model
+  })
+  const output = new StringOutputParser()
+
+  const chain = RunnableSequence.from([
+    prompt,
+    model,
+    output,
+  ])
+
+  const input = Object.fromEntries(
+      Object.entries({...source, ...args}
+    )
+    .map(([ key, value]) => [
+      key,
+      typeof value === 'object' ? JSON.stringify(value) : value
+    ]))
+
+  const res = await chain.invoke(input)
+
+  return { text: res }
+}
+
+const resolvers = {
+  Movie: {
+    generate,
+  },
+  Actor: {
+    generate,
+  },
+};
+
+// Assign resolver to many types
+const withGenerateResolver = (types = []) =>
+  Object.fromEntries(
+    types.map(type => [
+      type,
+      { generate }
+    ])
+  )
+
+  // Define schema
+const neoSchema = new Neo4jGraphQL({
+    typeDefs,
+    driver,
+    resolvers: withGenerateResolver(['Movie', 'Actor']),
+});
 
 // Create server
 const server = new ApolloServer({
   schema: await neoSchema.getSchema(),
+  resolvers,
 });
 
 // Listen
